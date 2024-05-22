351a07f33ff38e3ba3f9a67567d41de58514c353
Dhdhdhdhdhhxdhhdhdhdhdhdbdj <sussyman1234578@outlook.com>
2024-05-22 12:14:16 -0400

    Update config.js
    Signed-off-by: Dhdhdhdhdhhxdhhdhdhdhdhdbdj <sussyman1234578@outlook.com>

--- config.js
+++ config.js
@@ -6,5 +6,3 @@ const config = {
     },
     routes: true, // Change this to false if you just want to host a bare server.
     local: true, // Change this to false to disable local assets.
-  }
-  export default config
